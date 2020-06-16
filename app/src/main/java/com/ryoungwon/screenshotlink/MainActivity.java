0597417902a9f47682eda221d977f87ec8e95bd5
KimRyoungWon <kyo9907@naver.com>
2020-06-16 17:45:52 +0900

    commit

--- app/src/main/java/com/ryoungwon/screenshotlink/MainActivity.java
+++ app/src/main/java/com/ryoungwon/screenshotlink/MainActivity.java
@@ -2,13 +2,24 @@ package com.ryoungwon.screenshotlink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
     }
 }
